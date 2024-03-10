9ea4f64636ac392b3990a77af5339b1adccaff40
RomanEvgenovichLoboda <loboda.rom@gmail.com>
2024-03-10 11:46:32 +0200

    Chapter 3.11 Error creating bean

--- src/main/java/tacos/web/DesignTacoController.java
+++ src/main/java/tacos/web/DesignTacoController.java
@@ -1,6 +1,7 @@
 package tacos.web;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -9,48 +10,54 @@ import tacos.Ingredient.Type;
 import tacos.Taco;
 import tacos.TacoOrder;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 import javax.validation.Valid;
+
 import org.springframework.validation.Errors;
+import tacos.data.IngredientRepository;
 
 @Slf4j
 @Controller
 @RequestMapping("/design")
 @SessionAttributes("tacoOrder")
 public class DesignTacoController {
+    private final IngredientRepository ingredientRepo;
+
+    @Autowired
+    public DesignTacoController(IngredientRepository ingredientRepo) {
+        this.ingredientRepo = ingredientRepo;
+    }
+
     @ModelAttribute
-    public void addIngredientsToModel(Model model){
-        List<Ingredient> ingredients = Arrays.asList(
-                new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
-                new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
-                new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
-                new Ingredient("CARN", "Carnitas", Type.PROTEIN),
-                new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES),
-                new Ingredient("LETC", "Lettuce", Type.VEGGIES),
-                new Ingredient("CHED", "Cheddar", Type.CHEESE),
-                new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
-                new Ingredient("SLSA", "Salsa", Type.SAUCE),
-                new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
-        );
+    public void addIngredientsToModel(Model model) {
+        Iterable<Ingredient> ingredients = ingredientRepo.findAll();
         Type[] types = Ingredient.Type.values();
-        for(Type type : types){
-            model.addAttribute(type.toString().toLowerCase(),filterByType(ingredients,type));
+        for (Type type : types) {
+            model.addAttribute(type.toString().toLowerCase(),
+                    filterByType((List)ingredients, type));
         }
     }
+
+
     @ModelAttribute(name = "tacoOrder")
-    public TacoOrder order(){
+    public TacoOrder order() {
         return new TacoOrder();
     }
+
     @ModelAttribute(name = "taco")
-    public Taco taco(){
-        return  new Taco();
+    public Taco taco() {
+        return new Taco();
     }
+
     @GetMapping
-    public String showDesignForm(){
+    public String showDesignForm() {
         return "design";
     }
+
     @PostMapping
     public String processTaco(
             @Valid Taco taco, Errors errors,
@@ -62,7 +69,8 @@ public class DesignTacoController {
         log.info("Processing taco: {}", taco);
         return "redirect:/orders/current";
     }
-    private Iterable<Ingredient> filterByType(List<Ingredient> ingredients, Type type){
+
+    private Iterable<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
         return ingredients.stream().filter(x -> x.getType().equals(type)).collect(Collectors.toList());
     }
 }
--- src/main/java/tacos/web/OrderController.java
+++ src/main/java/tacos/web/OrderController.java
@@ -9,11 +9,17 @@ import lombok.extern.slf4j.Slf4j;
 import javax.validation.Valid;
 import org.springframework.validation.Errors;
 import tacos.TacoOrder;
+import tacos.data.OrderRepository;
+
 @Slf4j
 @Controller
 @RequestMapping("/orders")
 @SessionAttributes("tacoOrder")
 public class OrderController {
+    private OrderRepository orderRepo;
+    public OrderController(OrderRepository orderRepo) {
+        this.orderRepo = orderRepo;
+    }
     @GetMapping("/current")
     public String orderForm() {
         return "orderForm";
@@ -24,7 +30,7 @@ public class OrderController {
         if (errors.hasErrors()) {
             return "orderForm";
         }
-        log.info("Order submitted: {}", order);
+        orderRepo.save(order);
         sessionStatus.setComplete();
         return "redirect:/";
     }
